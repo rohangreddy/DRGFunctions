26271007ea71f8d7662d56de249962806bd79f63
rohangreddy <rohangreddy@gmail.com>
2020-11-01 16:12:29 -0500

    added DRGFunctions package files

---
+++ R/calc_stats.R
@@ -0,0 +1,31 @@
+#' Title
+#'
+#' This function calculates a passed in statistic (mean, median, or standard deviation)
+#' for average medicare payments of DRG data.
+#'
+#' @param df a dataframe
+#' @param x a string indicating the statistic to calculate. Choose either "mean",
+#' "median", or "sd"
+#'
+#' @return A tibble of the dataframe with \code{x}, the statistic of choice
+#' @export
+#'
+#' @import tidyverse
+#'
+#' @examples
+#'
+#' drg_data = read.csv("drg.csv")
+#'
+#' calc_stats(drg_data, "mean")
+calc_stats = function(df, x) {
+  ## pipe in dataframe
+  df %>%
+    ## choose relevant columns
+    select(DRG.Definition, Average.Medicare.Payments) %>%
+    ## group by DRG code
+    group_by(DRG.Definition) %>%
+    ## calculate the passed in statistic per DRG code
+    summarise(
+      Statistic = get(x)(Average.Medicare.Payments)
+    )
+}
---
+++ R/make_drg_box.R
@@ -0,0 +1,40 @@
+#' Title
+#'
+#' This function produces a ggplot boxplot of \code{variable} versus DRG Code
+#'
+#' @param df a dataframe
+#' @param variable a string for the y variable. Please choose between:
+#' "Average.Medicare.Payments", "Average.Total.PAyments", and "Average.Covered.Charges"
+#'
+#' @return A plot with \code{variable} versus DRG code
+#' @export
+#'
+#' @import tidyverse
+#'
+#' @examples
+#'
+#' drg_data = read.csv("drg.csv")
+#'
+#' make_drg_box(drg_data, "Average.Medicare.Payments")
+#'
+make_drg_box = function(df, variable) {
+  ## Pipe in dataframe
+  df %>%
+    ## Shorten to numeric DRG code
+    mutate(DRG.Definition = substring(DRG.Definition, 1, 3)) %>%
+    ## Initialize ggplot object of passed in variable vs DRG code,
+    ## ordered by decreasing payment value
+    ggplot(aes(x = reorder(DRG.Definition, -get(variable)),
+               y = get(variable))) +
+    ## Create boxplot
+    geom_boxplot() +
+    ## Modify x axis tick labels, remove legend
+    theme(axis.text.x = element_text(size = 7.5, angle = 60, hjust = 1.25),
+          legend.position = "none") +
+    ## Remove "." from passed in variable and set as y axis label
+    ylab(paste(gsub("\\.", " ", variable), "(dollars)")) +
+    ## Change x axis label
+    xlab(paste("DRG Code")) +
+    ## Add title according to passed in variable name
+    ggtitle(paste((gsub("\\.", " ", variable)), "by DRG Code"))
+}
