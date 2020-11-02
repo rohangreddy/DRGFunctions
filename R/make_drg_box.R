a301227b9785d6667ff9e9362c2323b1254d445b
rohangreddy <rohangreddy@gmail.com>
2020-11-01 20:24:40 -0500

    updated imports

--- R/make_drg_box.R
+++ R/make_drg_box.R
@@ -9,14 +9,8 @@
 #' @return A plot with \code{variable} versus DRG code
 #' @export
 #'
-#' @import magrittr
 #' @import dplyr
-#' @importFrom ggplot2 ggplot
-#' @importFrom ggplot2 geom_boxplot
-#' @importFrom ggplot2 theme
-#' @importFrom ggplot2 ylab
-#' @importFrom ggplot2 xlab
-#' @importFrom ggplot2 ggtitle
+#' @import ggplot2
 #'
 #' @examples
 #'
@@ -25,16 +19,14 @@
 #' make_drg_box(drg_data, "Average.Medicare.Payments")
 #'
 make_drg_box = function(df, variable) {
-  ## Pipe in dataframe
-  df %>%
-    ## Shorten to numeric DRG code
-    mutate(DRG.Definition = substring(DRG.Definition, 1, 3)) %>%
-    ## Initialize ggplot object of passed in variable vs DRG code,
-    ## ordered by decreasing payment value
-    ggplot(aes(x = reorder(DRG.Definition, -get(variable)),
-               y = get(variable))) +
+  ## Shorten to numeric DRG code
+  df = mutate(df, DRG.Definition = substring(DRG.Definition, 1, 3))
+  ## Initialize ggplot object of passed in variable vs DRG code,
+  ## ordered by decreasing payment value
+  ggplot(data = df, mapping = aes(x = reorder(DRG.Definition, -get(variable)),
+                                  y = get(variable))) +
     ## Create boxplot
-    geom_boxplot() +
+    geom_boxplot(data = df) +
     ## Modify x axis tick labels, remove legend
     theme(axis.text.x = element_text(size = 7.5, angle = 60, hjust = 1.25),
           legend.position = "none") +
