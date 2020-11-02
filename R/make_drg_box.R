eca84d2bd9d4fae1d8a9dfd7ea76d36d8aa19ddb
rohangreddy <rohangreddy@gmail.com>
2020-11-01 20:32:44 -0500

    updated imports

--- R/make_drg_box.R
+++ R/make_drg_box.R
@@ -9,7 +9,7 @@
 #' @return A plot with \code{variable} versus DRG code
 #' @export
 #'
-#' @importFrom dplyr mutate
+#' @import dplyr
 #' @import ggplot2
 #'
 #' @examples
@@ -20,20 +20,20 @@
 #'
 make_drg_box = function(df, variable) {
   ## Shorten to numeric DRG code
-  df = mutate(df, DRG.Definition = substring(DRG.Definition, 1, 3))
+  df = dplyr::mutate(df, DRG.Definition = substring(DRG.Definition, 1, 3))
   ## Initialize ggplot object of passed in variable vs DRG code,
   ## ordered by decreasing payment value
-  ggplot(data = df, mapping = aes(x = reorder(DRG.Definition, -get(variable)),
+  ggplot2::ggplot(data = df, mapping = aes(x = reorder(DRG.Definition, -get(variable)),
                                   y = get(variable))) +
-    ## Create boxplot
-    geom_boxplot(data = df) +
-    ## Modify x axis tick labels, remove legend
-    theme(axis.text.x = element_text(size = 7.5, angle = 60, hjust = 1.25),
-          legend.position = "none") +
-    ## Remove "." from passed in variable and set as y axis label
-    ylab(paste(gsub("\\.", " ", variable), "(dollars)")) +
-    ## Change x axis label
-    xlab(paste("DRG Code")) +
-    ## Add title according to passed in variable name
-    ggtitle(paste((gsub("\\.", " ", variable)), "by DRG Code"))
+  ## Create boxplot
+  ggplot2::geom_boxplot(data = df) +
+  ## Modify x axis tick labels, remove legend
+  ggplot2::theme(axis.text.x = element_text(size = 7.5, angle = 60, hjust = 1.25),
+        legend.position = "none") +
+  ## Remove "." from passed in variable and set as y axis label
+  ggplot2::ylab(paste(gsub("\\.", " ", variable), "(dollars)")) +
+  ## Change x axis label
+  ggplot2::xlab(paste("DRG Code")) +
+  ## Add title according to passed in variable name
+  ggplot2::ggtitle(paste((gsub("\\.", " ", variable)), "by DRG Code"))
 }
