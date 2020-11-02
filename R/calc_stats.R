82dec9007209388cea0158b85fe45aa8a0adb7f9
rohangreddy <rohangreddy@gmail.com>
2020-11-01 20:46:39 -0500

    properly included dependencies

--- R/calc_stats.R
+++ R/calc_stats.R
@@ -10,23 +10,25 @@
 #' @return A tibble of the dataframe with \code{x}, the statistic of choice
 #' @export
 #'
-#' @import tidyverse
-#' @importFrom magrittr %>%
+#' @import dplyr
+#'
 #'
 #' @examples
 #'
 #' drg_data = read.csv("drg.csv")
 #'
 #' calc_stats(drg_data, "mean")
+
+
 calc_stats = function(df, x) {
-  ## pipe in dataframe
-  df %>%
-    ## choose relevant columns
-    select(DRG.Definition, Average.Medicare.Payments) %>%
-    ## group by DRG code
-    group_by(DRG.Definition) %>%
-    ## calculate the passed in statistic per DRG code
-    summarise(
-      Statistic = get(x)(Average.Medicare.Payments)
-    )
+  ## choose relevant columns
+  df = dplyr::select(df, DRG.Definition, Average.Medicare.Payments)
+  ## group by DRG code
+  df = dplyr::group_by(df, DRG.Definition)
+  ## calculate the passed in statistic per DRG code
+  df = dplyr::summarise(df,
+                        Statistic = get(x)(Average.Medicare.Payments)
+  )
+  ## Show the tibble
+  df
 }
--- R/make_drg_box.R
+++ R/make_drg_box.R
@@ -18,22 +18,23 @@
 #'
 #' make_drg_box(drg_data, "Average.Medicare.Payments")
 #'
+
 make_drg_box = function(df, variable) {
   ## Shorten to numeric DRG code
   df = dplyr::mutate(df, DRG.Definition = substring(DRG.Definition, 1, 3))
   ## Initialize ggplot object of passed in variable vs DRG code,
   ## ordered by decreasing payment value
-  ggplot2::ggplot(data = df, mapping = aes(x = reorder(DRG.Definition, -get(variable)),
-                                  y = get(variable))) +
-  ## Create boxplot
-  ggplot2::geom_boxplot(data = df) +
-  ## Modify x axis tick labels, remove legend
-  ggplot2::theme(axis.text.x = element_text(size = 7.5, angle = 60, hjust = 1.25),
-        legend.position = "none") +
-  ## Remove "." from passed in variable and set as y axis label
-  ggplot2::ylab(paste(gsub("\\.", " ", variable), "(dollars)")) +
-  ## Change x axis label
-  ggplot2::xlab(paste("DRG Code")) +
-  ## Add title according to passed in variable name
-  ggplot2::ggtitle(paste((gsub("\\.", " ", variable)), "by DRG Code"))
+  ggplot2::ggplot(data = df, mapping = ggplot2::aes(x = reorder(DRG.Definition, -get(variable)),
+                                                    y = get(variable))) +
+    ## Create boxplot
+    ggplot2::geom_boxplot(data = df) +
+    ## Modify x axis tick labels, remove legend
+    ggplot2::theme(axis.text.x = ggplot2::element_text(size = 7.5, angle = 60, hjust = 1.25),
+                   legend.position = "none") +
+    ## Remove "." from passed in variable and set as y axis label
+    ggplot2::ylab(paste(gsub("\\.", " ", variable), "(dollars)")) +
+    ## Change x axis label
+    ggplot2::xlab(paste("DRG Code")) +
+    ## Add title according to passed in variable name
+    ggplot2::ggtitle(paste((gsub("\\.", " ", variable)), "by DRG Code"))
 }
